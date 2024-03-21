d6ed8d55d27afcfa503b2895d5295540359fbfaf
Jake Powers <jpowers1324@gmail.com>
2024-03-21 18:29:05 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
+let src = "http://csce242-assignment14-yx60.onrender.com/";
+/*let src = "http://localhost:3000/";*/
+
 const showCrafts = async () => {
-    let response = await fetch("http://csce242-assignment14-yx60.onrender.com/api/crafts");
-    let craftJSON = await response.json();
+    let craftJSON = await getJSON();
+    if (craftJSON == "") {
+        return;
+    }
+
     let craftDiv = document.getElementsByClassName("crafts-list")[0];
 
 
@@ -9,13 +15,25 @@ const showCrafts = async () => {
         craftItem.classList.add("craft-item");
 
         let craftImg = document.createElement("img");
-        craftImg.src = "http://csce242-assignment14-yx60.onrender.com/crafts/" + craft.image;
+        craftImg.src = src + "crafts/" + craft.image;
         craftItem.append(craftImg);
 
         craftDiv.append(craftItem);
     });
 
 };
+
+const getJSON = async () => {
+    try {
+        console.log(src + "api/crafts")
+        let response = await fetch(src + "api/crafts");
+        return await response.json();
+    } catch(error) {
+        console.log("error retrieving JSON");
+        return "";
+    }
+};
+
 
 window.onload = () => {
     showCrafts();
